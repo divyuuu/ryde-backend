2851f07cc71655c8bb1a18cee18514bf592fd95d
DivyanshuPant <divyanshu.pant117@gmail.com>
2026-05-19 00:11:03 +0530

    Refactored Signup request dto -Refactored Signup and Login end points in Authentication controllers with validations using @Valid and @NotBlank and @NotNULL -Added BcryptPasswordEncoder to encode the user password and use passwordEncoder.matches() to match the legit user.

--- src/main/java/com/github/divyuuu/geolocation/dto/SignupRequestDto.java
+++ src/main/java/com/github/divyuuu/geolocation/dto/SignupRequestDto.java
@@ -2,16 +2,24 @@ package com.github.divyuuu.geolocation.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.github.divyuuu.geolocation.model.Role;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class SignupRequestDto {
+public class SignUpRequestDto {
 
     @JsonProperty("confirmPassword")
+    @NotBlank(message = "Password is required.")
     private String password;
+    @NotBlank(message = "Email is required.")
     private String email;
+
+    @NotBlank(message = "Name is required.")
     private String name;
+
+    @NotNull(message = "Role is required.")
     private Role role;
 }
--- src/main/java/com/github/divyuuu/geolocation/service/impl/AuthenticationServiceImpl.java
+++ src/main/java/com/github/divyuuu/geolocation/service/impl/AuthenticationServiceImpl.java
@@ -1,22 +1,57 @@
 package com.github.divyuuu.geolocation.service.impl;
 
 
+import com.github.divyuuu.geolocation.dto.LoginRequestDto;
+import com.github.divyuuu.geolocation.dto.SignUpRequestDto;
 import com.github.divyuuu.geolocation.model.User;
 import com.github.divyuuu.geolocation.repository.UserRepository;
+import com.github.divyuuu.geolocation.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.github.divyuuu.geolocation.service.AuthenticationService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.sql.SQLException;
 
 @Service
 public class AuthenticationServiceImpl implements AuthenticationService{
     @Autowired
     UserRepository userRepository;
 
-    @Override
-    public String signup(User user){
-        userRepository.save(user);
+    @Autowired
+    PasswordEncoder passwordEncoder;
 
-        return "";
+    @Autowired
+    UserService userService;
+
+    @Override
+    public void signup(SignUpRequestDto request){
+        User newUser = new User();
+        newUser.setName(request.getName());
+        newUser.setEmail(request.getEmail());
+
+        String rawPassword = request.getPassword();
+        String encryptedPassword = passwordEncoder.encode(rawPassword);
+        newUser.setPassword(encryptedPassword);
+
+        newUser.setRole(request.getRole());
+        newUser.setRating(0.0);
+        newUser.setTotalRides(0);
+
+        userRepository.save(newUser);
+
+    }
+
+    @Override
+    public Boolean login(LoginRequestDto request) throws SQLException {
+        User user = userService.findUser(request.getEmail());
+
+        if(user == null){
+            throw new RuntimeException("No user find with email " + request.getEmail());
+        }
+
+        return passwordEncoder.matches(request.getPassword(), user.getPassword());
     }
 }
